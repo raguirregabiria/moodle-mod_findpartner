c8320fa6ca597001fd16226bd1aed12f27c6cbf7
manuelalfredocolladocenteno <47385182+manuelalfredocolladocenteno@users.noreply.github.com>
2020-03-02 18:30:48 +0100

    Primitive plugin autogenerated

--- findpartner/version.php
+++ findpartner/version.php
@@ -15,21 +15,17 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * System to find partners for group projects
+ * Plugin version and other meta-data are defined here.
  *
- * @package    findpartner
- * @copyright Rodrigo Aguirregabiria 2020  
- * @copyright Manuel ALfredo Collado Centeno 2020
- * @copyright GIETA, 2020 
- * @copyright Universidad Politecnica de Madrid (UPM)
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package     mod_findpartner
+ * @copyright   2020 Your Name <you@example.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die;
+defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020030200; // The current module version (Date: YYYYMMDDXX)
-$plugin->requires  = 2018051700; // Requires this Moodle version
-$plugin->cron      = 0;          // Period for cron to check this module (secs)
-$plugin->component = 'mod_findpartner'; // Full name of the plugin (used for diagnostics).
-$plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "0.01"; // User-friendly version number.
+$plugin->component = 'mod_findpartner';
+$plugin->release = '0.1.0';
+$plugin->version = 2020030200;
+$plugin->requires = 2019052000;
+$plugin->maturity = MATURITY_ALPHA;
